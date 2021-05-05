44ddd140b87d49cd1d4d068d256c6bdd4fa22cb2
Fred <frederico.fischer@stud.hslu.ch>
2021-05-05 19:40:55 +0200

    Create Map-and-Slice-Modul

---
+++ Code/Maps_and_Slices/slice.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Array
+	// - is not resizable
+	// - value type
+	//
+
+	// What are slices?
+	// - resizable arrays (darunter ist ein Array)
+	// - reference-type (array is value-type)
+
+	// Declaration
+	var mySlice1 []string
+
+	// Initialization
+	// with literal Syntax
+	var mySlice2 = []string{"This", "is", "a", "string", "slice"}
+	// with make() --> creates empty slice of length 5
+	var mySlice3 = make([]string, 5)
+
+	// Create Slice from array
+	var myArray = [4]int{1, 2, 3, 4}
+	var mySliceFromArray1 = myArray[1:3] // --> Gets elements of index 1 until 3 (without 3)
+	var mySliceFromArray2 = myArray[0:]  // --> Gets all elements from Array
+
+	// Append element into slice
+	mySliceFromArray2 = append(mySliceFromArray2, 3)
+
+	// Access and modify slice --> access with index like an array
+	mySliceFromArray2[0] = 10
+
+	// Length and Capacity of Slices
+	// Length: How many items are inside the slice
+	// Capacity: The capacity of the underlying Array
+	var myAwesomeSlice = make([]int, 0, 3) // Slice
+	for i := 0; i < 5; i++ {
+		myAwesomeSlice = append(myAwesomeSlice, i)
+		fmt.Printf("cap %v, len %v, %p\n", cap(myAwesomeSlice), len(myAwesomeSlice), myAwesomeSlice)
+	}
+
+	// Slice is a ReferenceType!
+	// If passed into a function, changes on it will also be recognized outside that function!
+
+	// Multidimensional Slices
+	var myMultiDimensionalSlice = [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 8},
+	}
+
+	// Iterating over a slice
+	var myIteratingSlice = []string{"One", "Two", "Three"}
+	for index, value := range myIteratingSlice {
+		fmt.Println(index, value)
+	}
+
+}
