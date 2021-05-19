1d5a574b73b9c8eba894cab04a7af41258ec0551
Oliver Werlen <oliverwerlen@bluewin.ch>
2021-05-19 19:35:22 +0200

    package

--- Code/modules/greetings/greetings.go
+++ Code/modules/greetings/greetings.go
@@ -1,4 +1,4 @@
-package modules
+package greetings
 
 import "fmt"
 
--- Code/modules/hello/hello.go
+++ Code/modules/hello/hello.go
@@ -1,1 +1,16 @@
-package modules
+package main
+
+import (
+	"fmt"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
+
+//go run .
+//https://golang.org/doc/tutorial/call-module-code
